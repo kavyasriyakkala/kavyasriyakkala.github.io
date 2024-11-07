372609fedb163c30d79df2e90fde8de830b5782b
kavyasriyakkala <kavyasri1yakkala@gmail.com>
2024-11-07 19:26:46 +0530

    Add json2html.js for GitHub Pages

--- json2html.js
+++ json2html.js
@@ -1,14 +1,10 @@
 export default function json2html(data) {
-    // Extract all unique keys from the data array to form table headers
     const columns = [...new Set(data.flatMap(Object.keys))];
   
-    // Start the HTML table string with the data-user attribute
     let html = `<table data-user="kavyasri1yakkala@gmail.com">`;
     
-    // Add table headers
     html += `<thead><tr>${columns.map(col => `<th>${col}</th>`).join('')}</tr></thead>`;
     
-    // Add table body rows
     html += `<tbody>`;
     data.forEach(row => {
       html += `<tr>${columns.map(col => `<td>${row[col] || ''}</td>`).join('')}</tr>`;
